00bebe0e78f54e744366b1be3c43dcb8089de73f
Ahmed Azier <67380716+azierahmed@users.noreply.github.com>
2024-09-21 17:34:38 +0300

    Creating an invoice plus the form

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,6 +8,7 @@ export default function Search() {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+  const placeholder = "Search invoices...";
 
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
@@ -29,7 +30,7 @@ export default function Search() {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        // placeholder={placeholder}
+        placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
